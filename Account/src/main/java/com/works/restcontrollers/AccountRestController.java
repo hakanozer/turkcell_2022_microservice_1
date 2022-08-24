8d5bf4d961644e1122fef53ac8da2771d223742e
Hakan ÖZER <hakanozer02@gmail.com>
2022-08-24 15:55:57 +0300

    2-Days

--- Account/src/main/java/com/works/restcontrollers/AccountRestController.java
+++ Account/src/main/java/com/works/restcontrollers/AccountRestController.java
@@ -3,6 +3,7 @@ package com.works.restcontrollers;
 import com.works.entities.Account;
 import com.works.services.AcccountService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,12 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/account")
+@Slf4j
 public class AccountRestController {
 
     final AcccountService aService;
 
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody Account account) {
+        if ( account.getEmail().equals("ali@mail.com") ) {
+            //int end = 1 / 0;
+        }
         return aService.login(account);
     }
 
